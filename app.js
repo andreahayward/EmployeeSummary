a92c9254ecc3a3107061731798692f057b198618
Andrea Hayward <andrea.teofilo@yahoo.com>
2020-10-29 17:10:05 -0400

    updates for HTML rendering

--- app.js
+++ app.js
@@ -110,7 +110,7 @@ function employeeQuestions() {
                 if (response.addlEmployees === "Yes") {
                     employeeQuestions();
                 } else {
-                    generatePage();
+                    display()
                     return;
                 }
             });
@@ -145,7 +145,7 @@ function employeeQuestions() {
                 if (response.addlEmployees === "Yes") {
                     employeeQuestions();
                 } else {
-                    generatePage();
+                    display()
                     return;
                 }
             });
@@ -153,54 +153,13 @@ function employeeQuestions() {
         });
 
 
+
+
     }
+    
 
-    function generatePage() {
-        let allCards = "";
-
-        employeeList.forEach(item => {
-            let cardString = item.createCard();
-            allCards += cardString;
-        });
-
-        let fullHTML = `
-        <!DOCTYPE html>
-        <html lang="en">
-        <head>
-            <meta charset="UTF-8" />
-            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-            <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-            
-            <link rel="stylesheet" 
-            href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
-            integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
-            crossorigin="anonymous"
-        />
-      <script
-         src="https://kit.fontawesome.com/ab3fd93a87.js"
-         crossorigin="anonymous"
-      ></script>
-
-      <title>Employee Roster</title>
-      </head>
-
-      <body>
-        <div class="container-fluid bg-danger text-center d-flex align-items-center justify-content-center" style="height: 20vh">
-        <div class="h1 text-white" style="display: inline-block;"> My Team </div>
-        </div>
-
-        <div class="container mt-5">
-            <!-- card group start -->
-            <div class="card-deck d-inline-flex justify-content-center">
-                ${allCards}
-            </div>
-            <! -- end -->
-        </div>
-        </body>
-        </html>
-        `;
-
-    fs.writeFile("./output/roster.html", fullHTML, function(err) {
+    function display() {
+        fs.writeFile("roster.html", render(employeeList), function(err) {
         if (err) {
             return console.log(err);
         }
